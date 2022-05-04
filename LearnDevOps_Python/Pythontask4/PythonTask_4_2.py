6e3621055c67f748f6df60b5b30a9d7ca51d0473
Andrei Rudchenko <andy1504roode@gmail.com>
2022-05-04 10:37:06 +0300

    ^updates

--- LearnDevOps_Python/Pythontask4/PythonTask_4_2.py
+++ LearnDevOps_Python/Pythontask4/PythonTask_4_2.py
@@ -5,32 +5,31 @@ t = turtle.Turtle()
 
 
 def circumRadius(length, n):
-    r = length/float(2*math.sin(360/2*n))
+    r = length/float(2*math.sin(math.pi/n))
     return r
 
-def side_a(length, r):
-    a = math.sqrt(r**2 - (0.5*length)**2)
-    return a
 
-def find_angle(a,c):
-    angle = math.degrees(math.sin(a/c))
+
+def find_angle(n):
+    high_angle = 360/n
+    angle = (180 - high_angle) / 2
     return angle
 
 def printPolygon(t_name,length,n):
-
+    #t_name.hideturtle()
     radius = circumRadius(length, n)
-    a = side_a(length,radius)
-    lt_angle = find_angle(a,radius)
+    lt_angle = find_angle(n)
     for i in range(n):
         t_name.fd(length)
-        t_name.lt(lt_angle)
+        t_name.lt(180-lt_angle)
         t_name.fd(radius)
-        t_name.lt(180-2*lt_angle)
+        t_name.lt(180-(360/n))
         t_name.fd(radius)
         t_name.pu()
-        t_name.lt(lt_angle)
+        t_name.lt(180-lt_angle)
         t_name.fd(length)
+        t_name.lt(360/n)
         t_name.pd()
 
-printPolygon(t,50,5)
+printPolygon(t,200,5)
 turtle.mainloop()
