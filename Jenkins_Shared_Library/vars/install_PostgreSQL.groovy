91d184c2e47aa08940eb27d71672bac445616f45
andyroode <104736077+andyroode@users.noreply.github.com>
2023-01-10 16:40:11 +0300

    Update install_PostgreSQL.groovy

--- Jenkins_Shared_Library/vars/install_PostgreSQL.groovy
+++ Jenkins_Shared_Library/vars/install_PostgreSQL.groovy
@@ -6,4 +6,5 @@ def call (Map config = [:]){
 	sh "sed -i 's/pgsql_db_name/$config.pgsql_db_name/g' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
 	sh "sed -i 's/pgsql_user_name/$config.pgsql_user_name/g' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
 	sh "sed -i 's/pgsql_pass/$config.pgsql_pass/g' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
+	ansiblePlaybook installation: 'Ansible', inventory: 'Ansible_for_Jenkins/hosts', playbook: 'Ansible_for_Jenkins/InstallPostgres13.yml'
 }
