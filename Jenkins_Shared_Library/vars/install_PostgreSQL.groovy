2a7a35332da17dbbe94b75ff443e78903eff41ce
andyroode <104736077+andyroode@users.noreply.github.com>
2023-01-10 16:38:30 +0300

    Update install_PostgreSQL.groovy

--- Jenkins_Shared_Library/vars/install_PostgreSQL.groovy
+++ Jenkins_Shared_Library/vars/install_PostgreSQL.groovy
@@ -1,9 +1,9 @@
 def call (Map config = [:]){
-	sh "sed 's/db_ip_address/$config.db_ip_address/i' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
-	sh "sed 's/db_ssh_user/$config.db_ssh_user/i' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
-	sh "sed 's/db_ssh_pass/$config.db_ssh_pass/i' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
-	sh "sed 's/db_sudo_pass/$config.db_sudo_pass/i' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
-	sh "sed 's/pgsql_db_name/$config.pgsql_db_name/i' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
-	sh "sed 's/pgsql_user_name/$config.pgsql_user_name/i' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
-	sh "sed 's/pgsql_pass/$config.pgsql_pass/i' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
+	sh "sed -i 's/db_ip_address/$config.db_ip_address/g' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
+	sh "sed -i 's/db_ssh_user/$config.db_ssh_user/g' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
+	sh "sed -i 's/db_ssh_pass/$config.db_ssh_pass/g' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
+	sh "sed -i 's/db_sudo_pass/$config.db_sudo_pass/g' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
+	sh "sed -i 's/pgsql_db_name/$config.pgsql_db_name/g' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
+	sh "sed -i 's/pgsql_user_name/$config.pgsql_user_name/g' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
+	sh "sed -i 's/pgsql_pass/$config.pgsql_pass/g' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml"
 }
