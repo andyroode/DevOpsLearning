2e97429fe59d9a18a2380ce245bce092dced7981
andyroode <104736077+andyroode@users.noreply.github.com>
2023-01-11 19:18:49 +0300

    Update install_Liferay.groovy

--- Jenkins_Shared_Library/vars/install_Liferay.groovy
+++ Jenkins_Shared_Library/vars/install_Liferay.groovy
@@ -1,5 +1,4 @@
 def call (Map config = [:]){
 	sh "sed -i 's/web_server_ip_address/$config.web_server_ip_address/g' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/web_server.yaml"
-	sh "sed -i 's/liferay_ssh_user_pass/$config.liferay_ssh_user_pass/g' ${pwd}/workspace/${JOB_NAME}/Ansible_for_Jenkins/host_vars/web_server.yaml"
 	ansiblePlaybook installation: 'Ansible', inventory: 'Ansible_for_Jenkins/hosts', playbook: 'Ansible_for_Jenkins/InstallLifeRay.yml'
 }
