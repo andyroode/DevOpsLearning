9d6e7baca7526a3e9cd1699ecef0395bfac44ec3
andyroode <104736077+andyroode@users.noreply.github.com>
2023-01-21 12:47:31 +0300

    Update install_PostgreSQL.groovy

--- Jenkins_Shared_Library/vars/install_PostgreSQL.groovy
+++ Jenkins_Shared_Library/vars/install_PostgreSQL.groovy
@@ -1,5 +1,5 @@
 def call (Map config = [:]){
-	def file = new File(${pwd}+'/workspace/{$JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml')
+	def file = new File(pwd+'/workspace/{$JOB_NAME}/Ansible_for_Jenkins/host_vars/db_server.yaml')
 	def newConfig = file.text.replace('db_ip_address', '${config.db_ip_address}').replace('pgsql_db_name', '${config.pgsql_db_name}').replace('db_ssh_pass', '${config.db_ssh_pass}')
     	file.text = newConfig
 	ansiblePlaybook disableHostKeyChecking: true, 
