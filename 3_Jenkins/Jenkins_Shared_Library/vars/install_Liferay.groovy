a5d6c9ed6205dd33a5950a71a775db327f58c99c
andyroode <104736077+andyroode@users.noreply.github.com>
2023-01-21 14:20:11 +0300

    Update install_Liferay.groovy

--- 3_Jenkins/Jenkins_Shared_Library/vars/install_Liferay.groovy
+++ 3_Jenkins/Jenkins_Shared_Library/vars/install_Liferay.groovy
@@ -1,5 +1,5 @@
 def call (Map config = [:]){
-	def file = new File(pwd+'/workspace/'+JOB_NAME+'/Ansible_for_Jenkins/host_vars/web_server.yaml')
+	def file = new File(pwd+'/workspace/'+JOB_NAME+'/2_Ansible/host_vars/web_server.yaml')
 	def new_web_serverConf = file.text.replace('web_server_ip_address',config.web_server_ip_address)
     	file.text = new_web_serverConf
 	ansiblePlaybook become: true, 
