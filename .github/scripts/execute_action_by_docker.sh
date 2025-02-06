2a3fa061094cec60c79b8dc80a18a842aac63655
Andrei Rudchenko <andrei.rudchenko.qubership@gmail.com>
2025-02-06 13:39:01 +0300

    Added new files

---
+++ .github/scripts/execute_action_by_docker.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+CONTAINER_IMAGE=${1?Error: no container image specified. Usage: ./run_gcip.sh <container_image> <function>}
+FUNCTION=${2?Error: no function/command specified. Usage: ./run_gcip.sh <container_image> <function>}
+
+docker run --rm \
+  -v "$GITHUB_WORKSPACE:/repo" \
+  -w /repo \
+  -e CI_PROJECT_DIR="/repo" \
+  -e ENV_NAMES="${ENV_NAMES:-""}" \
+  -e ENV_BUILDER="${ENV_BUILDER:-""}" \
+  -e GENERATE_EFFECTIVE_SET="${GENERATE_EFFECTIVE_SET:-""}" \
+  -e ENV_TEMPLATE_VERSION="${ENV_TEMPLATE_VERSION:-""}" \
+  -e ENV_TEMPLATE_TEST="${ENV_TEMPLATE_TEST:-""}" \
+  -e IS_OFFSITE="${IS_OFFSITE:-""}" \
+  -e JSON_SCHEMAS_DIR="${JSON_SCHEMAS_DIR:-""}" \
+  -e SD_DATA="${SD_DATA:-"{}"}" \
+  -e SD_VERSION="${SD_VERSION:-""}" \
+  -e ENV_INVENTORY_INIT="${ENV_INVENTORY_INIT:-""}" \
+  -e CI_COMMIT_REF_NAME="${CI_COMMIT_REF_NAME:-""}" \
+  "$CONTAINER_IMAGE" \
+  python /module/scripts/github_actions.py "$FUNCTION"
